585b720825c369ffa77ec5c87aeb165fd84d6867
Stas Moshchenskyi <spawn200994@gmail.com>
2019-11-10 14:52:56 +0300

    lesson 12

--- js/main.js
+++ js/main.js
@@ -14,20 +14,15 @@ window.addEventListener('DOMContentLoaded', function () {
                 seconds = Math.floor(timeRemaining % 60),
                 minutes = Math.floor(timeRemaining / 60) % 60,
                 hours = Math.floor(timeRemaining / 60 / 60);
+            console.log('Запуск таймера');
             return {timeRemaining, hours, minutes, seconds};
         }
-
         function updateClock () {
             let timer = getTimeRemaining();
-
             timerHours.textContent = timer.hours;
             timerMinutes.textContent = timer.minutes;
             timerSeconds.textContent = timer.seconds;
-            console.log(timerHours.textContent.length);
-
-
             if (timer.timeRemaining > 0) {
-                setTimeout(updateClock, 1000);
                 if (timerHours.textContent.length <= 1) {
                     timerHours.textContent = '0' + timer.hours;
                 }
@@ -37,15 +32,16 @@ window.addEventListener('DOMContentLoaded', function () {
                 if (timerSeconds.textContent.length <= 1) {
                     timerSeconds.textContent = '0' + timer.seconds;
                 }
-
             } else if (timer.timeRemaining <= 0) {
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
                 timerSeconds.textContent = '00';
             }
         }
+
         updateClock ();
+        setInterval(updateClock, 1000);
     }
 
-    countTimer('10 november 2019');
+    countTimer('11 november 2019');
 });
